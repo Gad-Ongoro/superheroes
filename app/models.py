799addaf65ee590430a50c0720412c04bd1b7e74
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-14 11:50:49 +0300

    Validations

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -29,6 +30,12 @@ class Power(db.Model, SerializerMixin):
     
     hero_powers = db.relationship('HeroPower', backref = 'power')
     
+    @validates(description)
+    def validate_description(self, key, value):
+        if (not value) and (len(value) < 20):
+            raise ValueError("Must add a description with at least 20 characters long")
+        return value
+    
 class HeroPower(db.Model, SerializerMixin):
     __tablename__ = 'hero_powers'
     
@@ -40,5 +47,12 @@ class HeroPower(db.Model, SerializerMixin):
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    @validates(strength)
+    def validate(self, key, value):
+        if 'Strong' not in value or 'Weak' not in value or 'Average' not in value:
+            raise ValueError('Must be a valid strength')
+        return value
+        pass
 
 # add any models you may need. 
